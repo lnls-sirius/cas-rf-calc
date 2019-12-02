ce4861f1bda44266028993f3ba4bcf1654d81b61
carneirofc <claudiofcarneiro@hotmail.com>
2019-12-02 09:57:08 -0300

    EPICS dependencies and base files for development.

--- iocBoot/ioccalc/st.cmd
+++ iocBoot/ioccalc/st.cmd
@@ -11,11 +11,20 @@ cd "${TOP}"
 dbLoadDatabase "dbd/calc.dbd"
 calc_registerRecordDeviceDriver pdbbase
 
+asSetFilename("${TOP}/db/Security.as")
+
 ## Load record instances
-#dbLoadRecords("db/xxx.db","user=carneirofc")
+dbLoadRecords("${TOP}/db/Calc.db","user=carneirofc")
+
+set_savefile_path("autosave")
+
+# Consts
+set_pass0_restoreFile("${TOP}/autosave/Calc.sav")
+set_pass1_restoreFile("${TOP}/autosave/Calc.sav")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
-## Start any sequence programs
-#seq sncxxx,"user=carneirofc"
+caPutLogInit "10.128.255.4:7011" 2
+
+create_monitor_set("${TOP}/db/Calc.req", 10)
