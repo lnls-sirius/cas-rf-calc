03395102bd7916fa73b0b3bbd7ff4f7d9277a3b7
carneirofc <claudiofcarneiro@hotmail.com>
2019-12-06 12:11:05 -0300

    WIP

--- calcApp/Db/gen.py
+++ calcApp/Db/gen.py
@@ -2,7 +2,12 @@
 import logging
 
 from string import Template
-
+trigger = Template('''
+record(ai, "${name}"){
+    field(INP, "${ref} MSS CP")
+    field(PREC, "${prec}")
+}
+''')
 flow_rate = Template('''
 record(ai, "${name}"){
     field(DESC, "${desc}") 
@@ -12,10 +17,16 @@ record(ai, "${name}"){
 ''')
 
 temp_delta = Template('''
+record(ai, "${name}_buffer"){
+    field(INP, "${temp} MSS")
+    field(SCAN, "Passive")
+    field(PREC, "${prec}")
+}
 record(calc, "${name}"){
-    field(CALC, "")
-    field(INPA, "")
-    field(INPB, "")
+    field(CALC, "B-A")
+    field(INPA, "${name}_buffer MSS NPP")
+    field(INPB, "${name}_buffer MSS PP")
+    field(INPC, "${trigger} CP")
     field(DESC, "${desc}") 
     field(PREC, "${prec}")
     field(EGU,  "${egu}")
@@ -25,8 +36,8 @@ record(calc, "${name}"){
 pwrdiss_n = Template('''
 record(calc, "${name}"){
     field(CALC, "1.16*A*B")
-    field(INPA, "${t_delta}")   # Disc or cell delta T
-    field(INPB, "${flow_rate}")
+    field(INPA, "${t_delta} CP")   # Disc or cell delta T
+    field(INPB, "${flow_rate} CP")
     field(DESC, "${desc}") 
     field(PREC, "${prec}")
     field(EGU,  "${egu}")
@@ -45,10 +56,10 @@ pwr_cell_n = Template('''
 record(calc, "${name}"){
     field(CALC, "(A/(1+E/C)+C+(B/(1+D/C)))")
     field(INPA, "${pwr_disc}.VAL[1]")   #  n
-    field(INPB, "${pwr_disc}.VAL[0]")   #  n+1
+    field(INPB, "${pwr_disc}.VAL[2]")   #  n+1
     field(INPC, "${pwr_cell}.VAL[1]")   #  n
-    field(INPD, "${pwr_cell}.VAL[0]")   #  n+1
-    field(INPE, "${pwr_cell}.VAL[2]")   #  n-1
+    field(INPD, "${pwr_cell}.VAL[2]")   #  n+1
+    field(INPE, "${pwr_cell}.VAL[0]")   #  n-1
     field(DESC, "${desc}") 
     field(PREC, "${prec}")
     field(EGU,  "${egu}")
@@ -68,19 +79,25 @@ record(calc,  "${name}"){
 
 if __name__ == '__main__':
     defaults = {'prec':'4', 'desc':'', 'egu':''}
+    trigger_name = 'RF_CALC_IOC_TRIGGER_'
+    trigger_ref_name = "SI-02SB:RF-P7Cav:WInT-Mon"
+
 
     db = ''
-    db += flow_rate.safe_substitute(defaults, name='flow_rate_1')
-    db += temp_delta.safe_substitute(defaults, name='temp_delta_cell_1')
-    db += temp_delta.safe_substitute(defaults, name='temp_delta_disc_1')
+    flow_rate_name = 'SI-02SB:RF-P7Cav:Disc1FlwRt-Mon'
+    temp_delta_name = 'SI-02SB:RF-P7Cav:Disc1WdT-Mon'
+    temp_name = 'SI-02SB:RF-P7Cav:Cylin1WT-Mon'
+    pwr_diss_name = 'SI-02SB:RF-P7Cav:PwrDissDisc1-Mon'
+    db += trigger.safe_substitute(defaults, name=trigger_name, ref=trigger_ref_name)
+    db += flow_rate.safe_substitute(defaults, name=flow_rate_name)
+    db += temp_delta.safe_substitute(defaults, name=temp_delta_name, temp=temp_name, trigger=trigger_name)
+    db += pwrdiss_n.safe_substitute(defaults, name=pwr_diss_name, flow_rate=flow_rate_name,t_delta=temp_delta_name)
+    #    t_delta='temp_delta_cell_1')
+    #db += pwrdiss_n.safe_substitute(defaults, name='pwrdiss_disc_1', flow_rate='flow_rate_1',
+    #    t_delta='temp_delta_disc_1')
 
-    db += pwrdiss_n.safe_substitute(defaults, name='pwrdiss_cell_1', flow_rate='flow_rate_1',
-        t_delta='temp_delta_cell_1')
-    db += pwrdiss_n.safe_substitute(defaults, name='pwrdiss_disc_1', flow_rate='flow_rate_1',
-        t_delta='temp_delta_disc_1')
-
-    db += pwr_cell_n.safe_substitute(defaults, name='pwr_cell_1', pwr_cell='pwrdiss_cell_1',
-        pwr_disc='pwrdiss_disc_1') 
+    #db += pwr_cell_n.safe_substitute(defaults, name='pwr_cell_1', pwr_cell='pwrdiss_cell_1',
+    #    pwr_disc='pwrdiss_disc_1')
 
     with open('Calc.db', 'w+') as _f:
         _f.write(db)
--- iocBoot/ioccalc/st.cmd
+++ iocBoot/ioccalc/st.cmd
@@ -19,12 +19,12 @@ dbLoadRecords("${TOP}/db/Calc.db","user=carneirofc")
 set_savefile_path("autosave")
 
 # Consts
-set_pass0_restoreFile("${TOP}/autosave/Calc.sav")
-set_pass1_restoreFile("${TOP}/autosave/Calc.sav")
+#set_pass0_restoreFile("${TOP}/autosave/Calc.sav")
+#set_pass1_restoreFile("${TOP}/autosave/Calc.sav")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
 caPutLogInit "10.128.255.4:7011" 2
 
-create_monitor_set("${TOP}/db/Calc.req", 10)
+#create_monitor_set("${TOP}/db/Calc.req", 10)
